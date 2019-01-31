eaade6623cd4d4823d56042a1080600b3a37407c
hegarnn <hegarnn@rose-hulman.edu>
2019-01-31 14:22:33 -0500

    123456789

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,7 +1,7 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
+Authors: Nasser Hegar and YOUR_PARTNERS_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
